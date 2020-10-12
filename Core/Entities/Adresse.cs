723d1a4899111eebc6ee4476a11d182648a1df57
ismailbenjemra <72107882+ismailbenjemra@users.noreply.github.com>
2020-10-13 01:22:39 +0200

    Ajoutez des fichiers projet.

---
+++ Core/Entities/Adresse.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Adresse :EntityBase
+    {
+        public string street { get; set; }
+        public string City { get; set; }
+        public int number { get; set; }
+    }
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    class Owner :EntityBase
+    {
+        public string FullName { get; set; }
+        //avatar,Profil
+        public string Avatar { get; set; }
+        public string Profil { get; set; }
+
+        public Adresse adresse { get; set; }
+    }
+}
